759b5e72c81d24d75e6004ab52517e1335fe0d2f
xhpwn <saxena20@purdue.edu>
2018-04-09 19:17:54 -0400

    Angular 5.X setup + Mongo connections

---
+++ assets/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'my-app',
+    template: `
+    <h1>TeamMaker</h1>
+`
+})
+export class AppComponent {
+}
---
+++ assets/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from "@angular/http";
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpModule
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+
+}
