971be1dc8699344cecffcd6a7205c63b83432e2a
xhpwn <saxena20@purdue.edu>
2018-04-17 10:17:30 -0400

    Collapse works, members list complete

--- assets/app/teams/team.component.ts
+++ assets/app/teams/team.component.ts
@@ -17,14 +17,14 @@ import { TeamService } from "./team.service";
             </div>
             <div class="config">
                 <a data-toggle="collapse" data-target="#teams" style="color: darkblue" (click)="onEdit()">Teams</a> | 
-                <a data-toggle="collapse" data-target="#members" style="color: darkblue">Members</a> | 
+                <a data-toggle="collapse" [attr.data-target]="'#' + team.teamName" style="color: darkblue">Members</a> | 
                 <a style="color: darkblue" (click)="onGenerate()">Generate</a>
             </div>
             <div style="padding-bottom: 2%;" id="teams" class="collapse">Teams
                 <div style="padding-top: 2%; color: red" *ngIf="!anyTeams()">No Teams yet! Hit the generate button after adding members!
                 </div>
             </div>
-            <div style="padding-bottom: 2%;" id="members" class="collapse">Members
+            <div style="padding-bottom: 2%;" id="{{team.teamName}}" class="collapse">Members
                 <div style="padding-top: 2%; padding-bottom: 2%; color: red" *ngIf="!anyMembers()">No Members yet! Add someone to this group below!
                 </div>
                 <div style="padding-top: 2%; padding-bottom: 2%; color: black" *ngIf="anyMembers()">
@@ -82,7 +82,7 @@ export class TeamComponent implements OnInit {
     }
 
     anyMembers() {
-        if (this.team.members != null)
+        if (this.team.members.length.toString() != '0')
             return true;
         return false;
     }
